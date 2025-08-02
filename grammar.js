e001e24630cf846926f22ac549e07bd00a881ff8
oscar-benderstone <oscarbenderstone@gmail.com>
2025-08-01 19:04:14 -0600

    Finished axioms and theorems

--- grammar.js
+++ grammar.js
@@ -17,16 +17,16 @@ module.exports = grammar({
   rules: {
     source_file: ($) => repeat($._statement),
 
-    _statement: ($) => choice($._sort_stmt, $._term_stmt),
+    _statement: ($) => choice($._sort_stmt, $._term_stmt, $._assert_stmt),
 
     // Sorts:
     // [https://github.com/digama0/mm0/blob/master/mm0.md#sorts]
     _sort_stmt: ($) =>
       seq(
-        // optional('pure'),
-        // optional('strict'),
-        // optional('provable'),
-        // optional('free'),
+        optional('pure'),
+        optional('strict'),
+        optional('provable'),
+        optional('free'),
         'sort',
         $.identifier,
       ),
@@ -52,18 +52,44 @@ module.exports = grammar({
 
     _identifier_: ($) => choice($.identifier, '_'),
 
+    // Axiom and theorems:
+    // [https://github.com/digama0/mm0/blob/master/mm0.md#axioms-and-theorems]
+    _assert_stmt: ($) =>
+      seq(
+        choice('axiom', 'theorem'),
+        $.identifier,
+        repeat($._formula_type_binder),
+        ':',
+        $._formula_arrow_type,
+        ';',
+      ),
+    _formula_type_binder: ($) =>
+      choice(
+        seq('{', repeat($.identifier), ':', $.type, '}'),
+        seq('(', repeat($._identifier_), ':', choice($.type, $._formula), ')'),
+      ),
+    _formula_arrow_type: ($) =>
+      choice(
+        $._formula,
+        seq(choice($.type, $._formula), '>', $._formula_arrow_type),
+      ),
+    _formula: ($) => $._math_string,
+
     // Lexical structure
     // [https://github.com/digama0/mm0/blob/master/mm0.md#lexical-structure]
-
     _lexeme: ($) => choice($._symbol, $.identifier, $.number, $._math_string),
     _symbol: ($) =>
       choice('*', '>', '.', ':', ';', '(', ')', '{', '}', '=', '_'),
 
+    _math_string: ($) => /\$[^\$]*\$/,
+
     // Adds math-string from secondary parsing:
     // [https://github.com/digama0/mm0/blob/master/mm0.md#secondary-parsing]
-    _math_string: ($) => choice($.math_lexeme, $._math_whitespace),
-    _math_whitespace: ($) => /( |\n)*/,
-    math_lexeme: ($) => choice($.math_lexeme, $.identifier, '(', ')'),
+    // Tree-sitter doesn't allow matching the empty string
+    // (except for the start symbol).
+    // _math_string: ($) => seq(repeat(choice($.math_lexeme, $._math_whitespace))),
+    // _math_whitespace: ($) => /( |\n)*/,
+    // math_lexeme: ($) => choice($.math_lexeme, $.identifier, '(', ')'),
 
     number: ($) => /0|[1-9][0-9]*/,
     identifier: ($) => /[a-zA-Z][a-zA-Z]*/,
